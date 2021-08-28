3ae68d726dea0c5ad8972d652c674a5b651d692d
Codegass <wchweichenhao@gmail.com>
2021-08-28 05:42:16 +0000

    running on github coodespace changes

--- hello_world/target/debug/hello_world.d
+++ hello_world/target/debug/hello_world.d
@@ -1,1 +1,1 @@
-/Users/WeiChenhao/Documents/Code/Github/RustGear/hello_world/target/debug/hello_world: /Users/WeiChenhao/Documents/Code/Github/RustGear/hello_world/src/main.rs
+/workspaces/RustGear/hello_world/target/debug/hello_world: /workspaces/RustGear/hello_world/src/main.rs
